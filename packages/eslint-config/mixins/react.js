7df01189fe864f217cedb97b0993a723ea80374e
ooyuo <doyounjeong0@gmail.com>
2024-07-21 19:08:54 +0900

    chore: adding lint rule

--- packages/eslint-config/mixins/react.js
+++ packages/eslint-config/mixins/react.js
@@ -31,6 +31,55 @@ module.exports = {
   ],
 
   rules: {
+    "no-relative-import-paths/no-relative-import-paths": [
+      "warn",
+      { allowSameFolder: true, rootDir: "src", prefix: "@" },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      // camelCase 변수, PascalCase 변수, UPPER_CASE 변수 허용
+      {
+        selector: "variable",
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+      },
+      // camelCase 함수, PascalCase 함수 허용
+      {
+        selector: "function",
+        format: ["camelCase", "PascalCase"],
+      },
+      // PascalCase 클래스, interfaces, type aliases, enums 허용
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+      // interface 앞에 I 사용 불가
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^I[A-Z]",
+          match: false,
+        },
+      },
+      // typeAlias 앞에 T 사용 불가
+      {
+        selector: "typeAlias",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^T[A-Z]",
+          match: false,
+        },
+      },
+      // typeParameter 앞에 T 사용 불가
+      {
+        selector: "typeParameter",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^T[A-Z]",
+          match: false,
+        },
+      },
+    ],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
